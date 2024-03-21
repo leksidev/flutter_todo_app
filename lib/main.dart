f4e0a4203c53616a4dc3dbcf291e98976710a0f8
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2024-03-21 16:28:32 +0300

    feat: add logging routes, fix talker provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_app/src/routing/router.dart';
+import 'package:flutter_todo_app/src/utils/talker_provider.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
   runApp(
@@ -8,15 +10,21 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   MainApp({super.key});
   final _appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: _appRouter.config(),
+      debugShowCheckedModeBanner: true,
+      routerConfig: _appRouter.config(
+        navigatorObservers: () => [
+          TalkerRouteObserver(
+            ref.watch(talkerProvider),
+          ),
+        ],
+      ),
       title: 'Flutter Demo',
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
